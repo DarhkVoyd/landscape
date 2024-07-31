672ec47aa0a12c1c857a2d35fa2eb822b433ec83
D V <divyadityasnaruka@gmail.com>
2024-07-31 15:17:13 +0530

    add initial generateLogo

--- scripts/update_tooling_data.js
+++ scripts/update_tooling_data.js
@@ -86,7 +86,7 @@ if (toolsCategory) {
     const logoFileName = `${tool.uniqueName.replace(/ /g, "-").replace(/\//g, "-").replace(/\|/g, "-")}.svg`;
     const logoFilePath = path.join("logos", logoFileName);
     if (!fs.existsSync(logoFilePath)) {
-      generateSvg(tool.uniqueName, logoFilePath);
+      generateLogo(tool.uniqueName, logoFilePath, tool.toolingType);
     }
 
     subcategory.items.push({
@@ -132,19 +132,133 @@ function saveYaml(data, filePath) {
   }
 }
 
-function generateSvg(name, filePath) {
-  const size = 550; // Adjusted width and height to make it a square
-  const maxNameLength = 10;
+function generateLogo(name, filePath) {
+  const fonts = [
+    "Impact",
+    "Arial",
+    "Verdana",
+    "Tahoma",
+    "Trebuchet MS",
+    "Times New Roman",
+    "Georgia",
+    "Courier New",
+    "Lucida Console",
+    "Monospace",
+  ];
 
-  if (name.length > maxNameLength) {
-    name = name.slice(0, maxNameLength) + "...";
+  const colors = [
+    "#1E88E5",
+    "#D32F2F",
+    "#7B1FA2",
+    "#0288D1",
+    "#388E3C",
+    "#FBC02D",
+    "#F57C00",
+    "#1976D2",
+    "#AFB42B",
+    "#5D4037",
+    "#0097A7",
+    "#C2185B",
+    "#303F9F",
+    "#7B1FA2",
+    "#F4511E",
+    "#00796B",
+    "#512DA8",
+    "#1976D2",
+    "#388E3C",
+    "#FBC02D",
+    "#F57C00",
+    "#9E9E9E",
+    "#BDBDBD",
+  ];
+
+  const size = 512;
+  const randomColor = () => colors[Math.floor(Math.random() * colors.length)];
+  const randomFont = () => fonts[Math.floor(Math.random() * fonts.length)];
+
+  let textColor;
+  let backgroundColor;
+  do {
+    textColor = randomColor();
+    backgroundColor = randomColor();
+  } while (textColor === backgroundColor);
+
+  const originalName = name
+    .split("/")
+    .pop()
+    .replace(/[-.,@]/g, " ")
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+
+  const designType = Math.floor(Math.random() * 5);
+  let svgContent;
+
+  switch (designType) {
+    case 0:
+      const shortName =
+        originalName.length > 1
+          ? originalName.slice(
+              0,
+              Math.min(Math.floor(Math.random() * 3) + 2, originalName.length),
+            )
+          : originalName;
+      svgContent = `
+      <svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+        <text x="50%" y="50%" font-size="200" font-family="${randomFont()}" font-weight="bold" text-anchor="middle" alignment-baseline="middle" fill="${textColor}">${shortName}</text>
+      </svg>`;
+      break;
+
+    case 1:
+      const words = originalName.split(" ");
+      const firstWord = words[0] || "";
+      const secondWord = words[1] || "";
+      svgContent = `
+      <svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+        <text x="50%" y="45%" font-size="${firstWord.length < 4 ? "200" : "140"}"  font-family="${randomFont()}" font-weight="bold" text-anchor="middle" alignment-baseline="middle" fill="${textColor}" dy="-.1em">${firstWord}</text>
+        <text x="50%" y="55%" font-size="${secondWord.length < 4 ? "200" : "140"}" font-family="${randomFont()}" font-weight="bold" text-anchor="middle" alignment-baseline="middle" fill="${textColor}" dy=".9em">${secondWord}</text>
+      </svg>`;
+      break;
+
+    case 2:
+      const firstWordBrand = originalName.split(" ")[0] || "";
+      svgContent = `
+      <svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+        <text x="50%" y="50%" font-size="${firstWordBrand.length < 4 ? "200" : "140"}" font-family="${randomFont()}" font-weight="bold" alignment-baseline="middle" text-anchor="middle" fill="${textColor}">${firstWordBrand}</text>
+      </svg>`;
+      break;
+
+    case 3:
+      svgContent = `
+      <svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+        <rect x="10%" y="30%" width="80%" height="5%" fill="${backgroundColor}" />
+        <rect x="10%" y="65%" width="80%" height="5%" fill="${backgroundColor}" />
+        <text x="50%" y="50%" font-size="96" font-family="${randomFont()}" font-weight="bold" text-anchor="middle" alignment-baseline="middle" fill="${textColor}">
+          ${originalName.split()[0]}
+        </text>
+      </svg>`;
+      break;
+
+    case 4:
+      const color = randomColor();
+      const cornerSize = size * 0.2; // 20% of the SVG size
+      svgContent = `
+      <svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
+        <!-- Top-left corner triangle -->
+        <polygon points="0,0 ${cornerSize},0 0,${cornerSize}" fill="${color}" />
+        <!-- Top-right corner triangle -->
+        <polygon points="${size},0 ${size - cornerSize},0 ${size},${cornerSize}" fill="${color}" />
+        <!-- Bottom-left corner triangle -->
+        <polygon points="0,${size} ${cornerSize},${size} 0,${size - cornerSize}" fill="${color}" />
+        <!-- Bottom-right corner triangle -->
+        <polygon points="${size},${size} ${size - cornerSize},${size} ${size},${size - cornerSize}" fill="${color}" />
+        <!-- Centered text -->
+        <text x="50%" y="50%" font-size="96" font-family="${randomFont()}" font-weight="bold" text-anchor="middle" alignment-baseline="middle" fill="${textColor}">
+          ${originalName.split()[0]}
+        </text>
+      </svg>`;
+      break;
   }
-
-  const svgContent = `
-  <svg width="${size}" height="${size}" xmlns="http://www.w3.org/2000/svg">
-    <rect width="100%" height="100%" fill="#fff" />
-    <text y="50%" font-size="64" text-anchor="middle" fill="#000" dy=".3em">${name}</text>
-  </svg>`;
 
   fs.writeFileSync(filePath, svgContent, "utf8");
 }
